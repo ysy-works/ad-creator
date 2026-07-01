20c2ab6396c86246e6a870abc09554e4b3086b79
songwoohyun <song.d.woohyun@gmail.com>
2026-07-01 17:48:31 +0900

    feat: 모델 연결 더미 함수 구현 (모델팀 코드 연결 예정)

---
+++ app/model/model.py
@@ -0,0 +1,8 @@
+from PIL import Image
+
+def generate_ad_image(product_image: Image.Image, prompt: str) -> Image.Image:
+    """
+    모델 팀이 이 함수를 채워줄 예정.
+    지금은 더미 결과(입력 이미지 그대로 반환)로 전체 흐름 테스트.
+    """
+    return product_image
